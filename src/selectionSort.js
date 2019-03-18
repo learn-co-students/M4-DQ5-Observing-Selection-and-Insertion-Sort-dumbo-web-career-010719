53a7e93820f61043d6508fb0e3153169196e1cca
rhuwellsamano <rhuwell.samano@gmail.com>
2019-03-18 10:03:11 -0400

    fort sort

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,21 @@
+// swap function helper
+function swap(arr, i, j) {
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for(var i = 0; i < arr.length; i++) {
+    var min = i;
+    for(var j = i + 1; j < arr.length; j++) {
+      if(arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if(i !== min) {
+      swap(arr, i, min);
+    }
+  }
+  return arr;
 }
